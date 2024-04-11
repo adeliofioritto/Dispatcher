38d9acc3d4def76c133d6205d0089f998195db5c
adeliofioritto <adelio.fioritto@gmail.com>
2024-04-11 11:14:02 +0200

    Update start.js

--- start.js
+++ start.js
@@ -104,7 +104,7 @@ async function scriviLogAppDB(msg) {
   console.log(msg);
 
   result = await connection.execute(
-    `INSERT INTO DISPATCHER_LOG_APP (time_stamp,function_name,message_body,instance_name,worker_id, process_id) VALUES (TO_DATE(:timestamp,'YYYY-MM-DD HH24:MI:SS'), :funzione, :messaggio, :instanceName, :processID )`,
+    `INSERT INTO DISPATCHER_LOG_APP (time_stamp,function_name,message_body,instance_name,worker_id, process_id) VALUES (TO_DATE(:timestamp,'YYYY-MM-DD HH24:MI:SS'), :funzione, :messaggio, :instanceName, :workerID, :processID )`,
     [msg.timestamp, msg.funzione, msg.messaggio, msg.instanceName, msg.workerID, msg.processID], { autoCommit: true }
   );
   //console.log("Rows inserted: " + result.rowsAffected);  // 1
